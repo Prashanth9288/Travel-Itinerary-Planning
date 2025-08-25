e590d899913afc98e98fa9fe6eaf777b67542834
uppara prashanth <prashanth939288@gmail.com>
2025-08-25 16:45:59 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // 👈 fixes MIME issues with asset loading
   build: {
-    outDir: "dist" // Vercel expects "dist"
+    outDir: "dist"
   },
   server: {
     port: 5173,
